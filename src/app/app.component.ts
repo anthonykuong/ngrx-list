15308f6d9183fefb27f78c04fb55c13e73252f9e
Anthony Ku Ong <anthonykuong@gmail.com>
2020-05-10 00:45:26 -0400

    updated with ngrx stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ShoppingItem} from './store/models/shopping-item.model';
+import {Store} from '@ngrx/store';
+import {AppState} from './store/models/app-state.model';
+import {AddItemAction, DeleteItemAction} from './store/actions/shopping.actions';
+import {Observable} from 'rxjs';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngrx-list';
+export class AppComponent implements OnInit {
+  shoppingItems: Observable<Array<ShoppingItem>>;
+  newShoppingItem: ShoppingItem = { id: '', name: '' }
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.shoppingItems = this.store.select(store => store.shopping);
+  }
+
+  addItem() {
+    this.newShoppingItem.id = uuid();
+
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+
+    this.newShoppingItem = { id: '', name: '' };
+  }
+
+  deleteItem(id: string) {
+    this.store.dispatch(new DeleteItemAction(id));
+  }
 }
